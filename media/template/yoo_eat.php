903b31028abddf03e091966991ae87f20a937d23
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-12-10 14:06:20 -0500

    updates

---
+++ media/template/yoo_eat.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "3296ea377394e24b9e1fd1a168f8dd30",
+    "tmpl_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 1.0.8 is currently the newest version available.\"}",
+    "warp_check": "21cd9d635e50bd953ec29daa871a583c",
+    "warp_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 7.3.15 is currently the newest version available.\"}"
+}
