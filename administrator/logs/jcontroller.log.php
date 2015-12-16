e9f4c0b3d222ab7b46296d74f422ffbb2213d994
camigreen <sgibbons@palmettoimages.com>
2015-12-16 15:58:25 -0500

    updates

---
+++ administrator/logs/jcontroller.log.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-12-16 19:44:53 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-12-16T19:44:53+00:00	INFO ::1	controller	Holding edit ID com_templates.edit.style.16 Array (     [0] => 16 ) 
+2015-12-16T19:44:53+00:00	INFO ::1	controller	Checking edit ID com_templates.edit.style.16: 1 Array (     [0] => 16 ) 
+2015-12-16T19:45:05+00:00	INFO ::1	controller	Releasing edit ID com_templates.edit.style.16 Array ( ) 
