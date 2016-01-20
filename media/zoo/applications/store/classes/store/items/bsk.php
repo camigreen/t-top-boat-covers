26e9353ccc93648d41805445d53ba3e73c7d15e3
camigreen <sgibbons@palmettoimages.com>
2016-01-20 14:20:23 -0500

    updates

---
+++ media/zoo/applications/store/classes/store/items/bsk.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author Shawn
+ */
+class BSKStoreItem extends StoreItem {
+
+    public $type = "bsk";
+    /**
+     * @var [string]
+     */
+    public $make = "LaPorte's T-Top Boat Covers";
+
+   	/**
+   	 * @var [string]
+   	 */
+   	public $model;
+   	
+    
+
+    public function importItem($item = null) {
+        parent::importItem($item);
+    }
+    
+
+}
+
+
+?>
